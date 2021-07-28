6abadba0c51f5b12df715a5595b62dac467d3f4b
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-07-28 10:59:16 +1000

    Added EmptySchema.

---
+++ Sources/Attributes/EmptySchema.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  File
+//
+//  Created by Morgan McColl on 28/7/21.
+//
+
+import Foundation
+
+struct EmptySchema<RootType: Modifiable>: SchemaProtocol {
+
+    typealias Root = RootType
+    
+}
