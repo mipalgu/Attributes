dfba698b8ee80d7df53229b15d0e4a74c525112b
Callum McColl <callum.mccoll1@gmail.com>
2021-09-12 04:43:58 +1000

    Use github for swift_helpers

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "ssh://git.mipal.net/Users/Shared/git/XMI.git", .branch("master")),
-        .package(url: "ssh://git.mipal.net/Users/Shared/git/swift_helpers.git", .branch("master")),
+        .package(url: "https://github.com/mipalgu/swift_helpers.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
