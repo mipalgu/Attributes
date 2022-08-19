af7456fec423a6ff68c40fe90309705214ff5f5a
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-08-19 20:06:29 +1000

    Added comments and tried to get linter to pass.

--- Sources/Attributes/Path.swift
+++ Sources/Attributes/Path.swift
@@ -148,68 +148,126 @@ extension ReadOnlyPath: Equatable, Hashable {
 
 }
 
+/// Path that points to values that can be read and written to.
 @dynamicMemberLookup
 public struct Path<Root, Value>: PathProtocol {
 
+    /// The ancestors of this path. These paths contain properties that contains this paths value.
     public var ancestors: [AnyPath<Root>]
 
+    /// A WriteableKeyPath equivalent type of this Path.
     public var path: WritableKeyPath<Root, Value>
 
+    /// A function for discerning whether the path points to a value that is nil in
+    /// a given Root object.
     private let _isNil: (Root) -> Bool
 
+    /// Create a read-only equivalent path.
+    public var readOnly: ReadOnlyPath<Root, Value> {
+        ReadOnlyPath(keyPath: self.path as KeyPath<Root, Value>, ancestors: self.ancestors)
+    }
+
+    /// Initialise this object with a WriteableKeyPath and a custom function that checks for nil values
+    /// pointed to by the given key path.
+    /// - Parameters:
+    ///   - path: The WriteableKeyPath pointing to the value this path points to.
+    ///   - ancestors: The ancestors of this path.
+    ///   - isNil: A function that checks for the presence of nil values in an object.
     init(path: WritableKeyPath<Root, Value>, ancestors: [AnyPath<Root>], isNil: @escaping (Root) -> Bool) {
         self.path = path
         self.ancestors = ancestors.reversed().drop { $0.partialKeyPath == path }.reversed()
         self._isNil = { root in ancestors.last?.isNil(root) ?? false || isNil(root) }
     }
 
+    /// Initialise this object with a path that points to a non-optional value.
+    /// - Parameters:
+    ///   - path: The path this object is initialised from.
+    ///   - ancestors: The ancestors of path.
     public init(path: WritableKeyPath<Root, Value>, ancestors: [AnyPath<Root>]) {
-        self.init(path: path, ancestors: ancestors, isNil: { _ in false })
+        self.init(path: path, ancestors: ancestors) { _ in false }
     }
 
-    public init<T>(path: WritableKeyPath<Root, Value>, ancestors: [AnyPath<Root>]) where Value == Optional<T> {
-        self.init(path: path, ancestors: ancestors, isNil: { root in root[keyPath: path] == nil })
+    /// Initialise this object from a path that points to an optional value.
+    /// - Parameters:
+    ///   - path: The path this object is initialised from.
+    ///   - ancestors: The ancestors of path.
+    public init<T>(
+        path: WritableKeyPath<Root, Value>, ancestors: [AnyPath<Root>]
+    ) where Value == T? {
+        self.init(path: path, ancestors: ancestors) { root in root[keyPath: path] == nil }
     }
 
+    /// Initialise self from the type of Value. This initialiser sets value as the Root and
+    /// the destination of the key path. This path is basically equivalent to \Value.self.
+    /// - Parameter type: The type that this path points to.
     public init(_ type: Value.Type) where Root == Value {
         self.init(path: \.self, ancestors: [])
     }
 
-    public var readOnly: ReadOnlyPath<Root, Value> {
-        return ReadOnlyPath(keyPath: self.path as KeyPath<Root, Value>, ancestors: self.ancestors)
-    }
-
-    public subscript<AppendedValue>(dynamicMember member: KeyPath<Value, AppendedValue>) -> ReadOnlyPath<Root, AppendedValue> {
-        return ReadOnlyPath<Root, AppendedValue>(keyPath: path.appending(path: member), ancestors: fullPath)
-    }
-
-    public subscript<AppendedValue>(dynamicMember member: WritableKeyPath<Value, AppendedValue>) -> Path<Root, AppendedValue> {
-        return Path<Root, AppendedValue>(path: path.appending(path: member), ancestors: fullPath)
-    }
-
+    /// Append a path to this Path.
+    /// - Parameter path: The path to append to self.
+    /// - Returns: A new path pointing from Root to the given paths values.
     public func appending<NewValue>(path: Path<Value, NewValue>) -> Path<Root, NewValue> {
         path.changeRoot(path: self)
     }
 
-    public func changeRoot<Prefix: PathProtocol>(path: Prefix) -> Path<Prefix.Root, Value> where Prefix.Value == Root {
+    /// Change the Root of this path. This function is equivalent to prepending a path to self.
+    /// This method acts as a pure function by creating a new path with the result.
+    /// - Parameter path: The path to prepend to self.
+    /// - Returns: A new path pointing from the path Root to self's Value.
+    public func changeRoot<Prefix: PathProtocol>(
+        path: Prefix
+    ) -> Path<Prefix.Root, Value> where Prefix.Value == Root {
         let ancestors = path.ancestors + self.ancestors.map {
             $0.changeRoot(path: path.readOnly)
         }
         return Path<Prefix.Root, Value>(path: path.path.appending(path: self.path), ancestors: ancestors)
     }
 
+    /// Checks whether the value pointed to by this path is nil in a given object.
+    /// - Parameter root: The object containing the value pointed to be this Path.
+    /// - Returns: Whether the value is nil in root.
     public func isNil(_ root: Root) -> Bool {
-        return self._isNil(root)
+        self._isNil(root)
+    }
+
+    /// Append to this path with a keyPath. This operation acts as a pure function by returning a new
+    /// Path with the keyPath appended.
+    /// - Parameter dynamicMember: The keyPath to append to this path.
+    /// - Returns: A new path with the keyPath appended to self.
+    public subscript<AppendedValue>(
+        dynamicMember member: KeyPath<Value, AppendedValue>
+    ) -> ReadOnlyPath<Root, AppendedValue> {
+        ReadOnlyPath<Root, AppendedValue>(keyPath: path.appending(path: member), ancestors: fullPath)
+    }
+
+    /// Append a WriteableKeyPath to self. This function creates a new Path pointing from self's Root to
+    /// the new Path's value.
+    /// - Parameter dynamicMember: The WriteableKeyPath to be appended to self.
+    /// - Returns: A new Path with dynamicMember appended to self.
+    public subscript<AppendedValue>(
+        dynamicMember member: WritableKeyPath<Value, AppendedValue>
+    ) -> Path<Root, AppendedValue> {
+        Path<Root, AppendedValue>(path: path.appending(path: member), ancestors: fullPath)
     }
 
 }
 
-extension Path {
+/// Equatable and Hashable conformance.
+extension Path: Equatable, Hashable {
 
-    public static func ==(lhs: Path<Root, Value>, rhs: Path<Root, Value>) -> Bool {
-        return lhs.ancestors == rhs.ancestors && lhs.keyPath == rhs.keyPath
+    /// Perform and equality operation by using value-equality. This function compares the ancestors
+    /// and keyPaths of the Path objects.
+    /// - Parameters:
+    ///   - lhs: The Path on the left-hand side of the == operator.
+    ///   - rhs: The Path on the right-hand side of the == operator.
+    /// - Returns: Whether lhs is equal to rhs.
+    public static func == (lhs: Path<Root, Value>, rhs: Path<Root, Value>) -> Bool {
+        lhs.ancestors == rhs.ancestors && lhs.keyPath == rhs.keyPath
     }
 
+    /// Define the members included in the hashing function for this type.
+    /// - Parameter hasher: The hasher that performs the hashing function.
     public func hash(into hasher: inout Hasher) {
         hasher.combine(self.ancestors)
         hasher.combine(self.keyPath)
@@ -217,18 +275,28 @@ extension Path {
 
 }
 
+/// Add validate function to Path.
 extension Path {
 
-    public func validate(@ValidatorBuilder<Root> builder: (ValidationPath<Path<Root, Value>>) -> AnyValidator<Root>) -> AnyValidator<Root> {
-        return builder(ValidationPath(path: Path(path: self.path, ancestors: self.fullPath)))
+    public func validate(
+        @ValidatorBuilder<Root> builder: (
+            ValidationPath<Path<Root, Value>>
+        ) -> AnyValidator<Root>
+    ) -> AnyValidator<Root> {
+        builder(ValidationPath(path: Path(path: self.path, ancestors: self.fullPath)))
     }
 
 }
 
+/// Add trigger function to Path.
 extension Path {
 
-    public func trigger(@TriggerBuilder<Root> builder: (WhenChanged<Path<Root, Value>, IdentityTrigger<Root>>) -> [AnyTrigger<Root>]) -> AnyTrigger<Root> {
-        return AnyTrigger(builder(WhenChanged(Path(path: self.path, ancestors: self.fullPath))))
+    public func trigger(
+        @TriggerBuilder<Root> builder: (
+            WhenChanged<Path<Root, Value>, IdentityTrigger<Root>>
+        ) -> [AnyTrigger<Root>]
+    ) -> AnyTrigger<Root> {
+        AnyTrigger(builder(WhenChanged(Path(path: self.path, ancestors: self.fullPath))))
     }
 
 }
