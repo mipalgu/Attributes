758efd379f2a8459913437f841c8c442b6a2d7d5
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-08-21 03:50:50 +1000

    Added comments.

--- Sources/Attributes/properties/TextProperty.swift
+++ Sources/Attributes/properties/TextProperty.swift
@@ -56,22 +56,32 @@
  *
  */
 
+/// A Text Property.
 @propertyWrapper
 public struct TextProperty {
 
+    /// A self property.
     public var projectedValue: TextProperty {
         self
     }
 
+    /// The underlying SchemaAttribute.
     public var wrappedValue: SchemaAttribute
 
+    /// Create the Property from a SchemaAttribute.
+    /// - Parameter wrappedValue: The attribute.
     public init(wrappedValue: SchemaAttribute) {
         self.wrappedValue = wrappedValue
     }
 
+    /// Create the property from a label and builder function.
+    /// - Parameters:
+    ///   - label: The label of this property.
+    ///   - builder: A function that creates the validator.
     public init(
         label: String,
-        @ValidatorBuilder<Attribute> validation builder: (ValidationPath<ReadOnlyPath<Attribute, String>>) -> AnyValidator<Attribute> = { _ in AnyValidator([]) }
+        @ValidatorBuilder<Attribute> validation builder: (ValidationPath<ReadOnlyPath<Attribute, String>>)
+            -> AnyValidator<Attribute> = { _ in AnyValidator([]) }
     ) {
         let path = ReadOnlyPath(keyPath: \Attribute.self, ancestors: []).blockAttribute.textValue
         let validationPath = ValidationPath(path: path)
