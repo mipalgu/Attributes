fcd467577a4621616c63d1c6ba6987dff02d1c9f
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-11-20 15:37:12 +1000

    Made docC plugin only get included when on Linux.

--- Package.swift
+++ Package.swift
@@ -3,29 +3,43 @@
 
 import PackageDescription
 
+/// Additional dependencies for specific architectures.
+let dependencies: [Package.Dependency]
+
+#if os(Linux)
+    dependencies = [.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")]
+#else
+    dependencies = []
+#endif
+
+/// Package manifest.
 let package = Package(
     name: "Attributes",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make
+        // them visible to other packages.
         .library(
             name: "Attributes",
-            targets: ["Attributes"]),
+            targets: ["Attributes"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "git@github.com:mipalgu/XMI.git", branch: "main"),
-        .package(url: "https://github.com/mipalgu/swift_helpers.git", branch: "main"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
-    ],
+        .package(url: "https://github.com/mipalgu/swift_helpers.git", branch: "main")
+    ] + dependencies,
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        // Targets are the basic building blocks of a package. A target can define a
+        // module or a test suite. Targets can depend on other targets in this package,
+        // and on products in packages this package depends on.
         .target(
             name: "Attributes",
-            dependencies: ["XMI", "swift_helpers"]),
+            dependencies: ["XMI", "swift_helpers"]
+        ),
         .testTarget(
             name: "AttributesTests",
-            dependencies: ["Attributes"]),
+            dependencies: ["Attributes"]
+        )
     ]
 )
