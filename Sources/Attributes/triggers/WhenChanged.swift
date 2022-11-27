fe22ca5ffdcd820539874539396f9b80ba5f8ed8
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-11-27 17:42:54 +1000

    Added documentation.

--- Sources/Attributes/triggers/WhenChanged.swift
+++ Sources/Attributes/triggers/WhenChanged.swift
@@ -56,25 +56,43 @@
  *
  */
 
+/// A trigger that initiates another trigger when a specific value is changed. This
+/// trigger will monitor a value through a path and initiate a trigger function when the
+/// value changes.
 public struct WhenChanged<
     Path: ReadOnlyPathProtocol, Trigger: TriggerProtocol
 >: TriggerProtocol where Path.Root == Trigger.Root {
 
+    /// The Root containing the value that is monitored.
     public typealias Root = Path.Root
 
+    /// The path to the value that fires this trigger.
     private let actualPath: Path
 
+    /// The trigger that is enacted when this trigger is fired.
     private let trigger: Trigger
 
+    /// A type-erased version of `actualPath`.
     public var path: AnyPath<Root> {
         AnyPath(actualPath)
     }
 
+    /// Initialise this trigger with a path to a monitored value and the trigger to
+    /// use when that value changes.
+    /// - Parameters:
+    ///   - actualPath: A path to a value that causes this trigger to fire.
+    ///   - trigger: The trigger enacted when this trigger fires.
     init(actualPath: Path, trigger: Trigger) {
         self.actualPath = actualPath
         self.trigger = trigger
     }
 
+    /// Performs the trigger function within the `trigger` property.
+    /// - Parameters:
+    ///   - root: The root object.
+    ///   - path: The path to the value within `root` that is causing this trigger
+    /// to fire.
+    /// - Returns: The result from the `trigger`'s `performTrigger` function. 
     public func performTrigger(
         _ root: inout Path.Root, for path: AnyPath<Root>
     ) -> Result<Bool, AttributeError<Path.Root>> {
@@ -84,42 +102,106 @@ public struct WhenChanged<
         return trigger.performTrigger(&root, for: path)
     }
 
+    /// Check whether a path causes this trigger to fire.
+    /// - Parameters:
+    ///   - path: The path to check.
+    ///   - _: Unused. Preserved for protocol conformance.
+    /// - Returns: Whether `path` causes this trigger to fire.
     public func isTriggerForPath(_ path: AnyPath<Path.Root>, in _: Root) -> Bool {
         path.isChild(of: self.path) || path.isSame(as: self.path)
     }
 
 }
 
+/// Add common triggers.
 extension WhenChanged where Trigger == IdentityTrigger<Path.Root> {
 
+    /// Creates a trigger that is fired when a value changes. This trigger does nothing
+    /// and will always succeed.
+    /// - Parameter path: The path the causes this trigger to fire.
+    /// - SeeAlso: ``IdentityTrigger``.
     public init(_ path: Path) {
         self.init(actualPath: path, trigger: IdentityTrigger())
     }
 
-    public func when<NewTrigger: TriggerProtocol>(_ condition: @escaping (Root) -> Bool, @TriggerBuilder<Root> then builder: (WhenChanged<Path, Trigger>) -> NewTrigger) -> WhenChanged<Path, ConditionalTrigger<NewTrigger>> where NewTrigger.Root == Root {
+    /// Creates a trigger that performs many trigger functions created from a ``TriggerBuilder``. The newly
+    /// created trigger will only fire when a condition is `true`.
+    /// - Parameters:
+    ///   - condition: The condition that causes the new trigger to fire.
+    ///   - builder: The trigger functions that are performed by the new trigger.
+    /// - Returns: The new trigger.
+    public func when<NewTrigger: TriggerProtocol>(
+        _ condition: @escaping (Root) -> Bool,
+        @TriggerBuilder<Root> then builder: (WhenChanged<Path, Trigger>) -> NewTrigger
+    ) -> WhenChanged<Path, ConditionalTrigger<NewTrigger>> where NewTrigger.Root == Root {
         WhenChanged<Path, ConditionalTrigger<NewTrigger>>(
             actualPath: actualPath,
             trigger: ConditionalTrigger(condition: condition, trigger: builder(self))
         )
     }
 
-    public func sync<TargetPath: SearchablePath>(target: TargetPath) -> SyncTrigger<Path, TargetPath> where TargetPath.Root == Root, TargetPath.Value == Path.Value {
+    /// Creates a ``SyncTrigger`` that updates a target path from the path stored in this trigger.
+    /// - Parameter target: The target path to update.
+    /// - Returns: A ``SyncTrigger`` that updated `target` from `self.actualPath`.
+    /// - SeeAlso: ``SyncTrigger``.
+    public func sync<TargetPath: SearchablePath>(
+        target: TargetPath
+    ) -> SyncTrigger<Path, TargetPath> where TargetPath.Root == Root, TargetPath.Value == Path.Value {
         SyncTrigger(source: actualPath, target: target)
     }
 
-    public func sync<TargetPath: SearchablePath>(target: TargetPath, transform: @escaping (Path.Value, TargetPath.Value) -> TargetPath.Value) -> SyncWithTransformTrigger<Path, TargetPath> where TargetPath.Root == Root {
+    /// Creates a ``SyncTrigger`` that update a target path from a the path stored in this trigger.
+    /// In addition to this, the value located at `self.actualPath` is transformed using a
+    /// transformation function.
+    /// - Parameter target: The target to update.
+    /// - Parameter transform: The transformation that transforms the value at `actualPath` to a new value
+    /// in the target.
+    /// - Returns: A new ``SyncTrigger``.
+    /// - SeeAlso: ``SyncTrigger``.
+    public func sync<TargetPath: SearchablePath>(
+        target: TargetPath, transform: @escaping (Path.Value, TargetPath.Value) -> TargetPath.Value
+    ) -> SyncWithTransformTrigger<Path, TargetPath> where TargetPath.Root == Root {
         SyncWithTransformTrigger(source: actualPath, target: target, transform: transform)
     }
 
-    public func makeAvailable<FieldsPath: PathProtocol, AttributesPath: PathProtocol>(field: Field, after order: [String], fields: FieldsPath, attributes: AttributesPath) -> MakeAvailableTrigger<Path, FieldsPath, AttributesPath> where FieldsPath.Root == Root, FieldsPath.Value == [Field], AttributesPath.Value == [String: Attribute] {
-        MakeAvailableTrigger(field: field, after: order, source: self.actualPath, fields: fields, attributes: attributes)
+    /// Creates a ``MakeAvailableTrigger`` with a source specified by this objects `actualPath`.
+    /// - Parameters:
+    ///   - field: The new field.
+    ///   - order: The fields to place immediately after the new field.
+    ///   - fields: A path to the fields property in the root object.
+    ///   - attributes: A path to the attributes property in the root object.
+    /// - Returns: A new ``MakeAvailableTrigger``.
+    /// - SeeAlso: ``MakeAvailableTrigger``.
+    public func makeAvailable<FieldsPath: PathProtocol, AttributesPath: PathProtocol>(
+        field: Field, after order: [String], fields: FieldsPath, attributes: AttributesPath
+    ) -> MakeAvailableTrigger<Path, FieldsPath, AttributesPath> where
+        FieldsPath.Root == Root,
+        FieldsPath.Value == [Field],
+        AttributesPath.Value == [String: Attribute] {
+        MakeAvailableTrigger(
+            field: field, after: order, source: self.actualPath, fields: fields, attributes: attributes
+        )
     }
 
-    public func makeUnavailable<FieldsPath: PathProtocol>(field: Field, fields: FieldsPath) -> MakeUnavailableTrigger<Path, FieldsPath> where FieldsPath.Root == Root, FieldsPath.Value == [Field] {
+    /// Create a ``MakeUnavailableTrigger`` with a source specified by this objects `actualPath`.
+    /// - Parameters:
+    ///   - field: The field to make unavailable.
+    ///   - fields: A path to the fields property in the root object.
+    /// - Returns: A new ``MakeUnavailableTrigger``.
+    /// - SeeAlso: ``MakeUnavailableTrigger``.
+    public func makeUnavailable<FieldsPath: PathProtocol>(
+        field: Field, fields: FieldsPath
+    ) -> MakeUnavailableTrigger<Path, FieldsPath> where FieldsPath.Root == Root, FieldsPath.Value == [Field] {
         MakeUnavailableTrigger(field: field, source: actualPath, fields: fields)
     }
 
-    public func custom(_ trigger: @escaping (inout Root) -> Result<Bool, AttributeError<Root>>) -> CustomTrigger<Path> {
+    /// Creates a ``CustomTrigger`` that is triggered by changing the value located at `actualPath`.
+    /// - Parameter trigger: The custom trigger function to enact when the new trigger fires.
+    /// - Returns: A new ``CustomTrigger``.
+    /// - SeeAlso: ``CustomTrigger``.
+    public func custom(
+        _ trigger: @escaping (inout Root) -> Result<Bool, AttributeError<Root>>
+    ) -> CustomTrigger<Path> {
         CustomTrigger(path: actualPath, trigger: trigger)
     }
 
