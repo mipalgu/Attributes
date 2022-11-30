1c10633365e28d570c5bee692df7478389c22b13
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-11-30 17:18:51 +1000

    renamed greylist to greyList.

--- Sources/Attributes/validators/ValidatorFactory.swift
+++ Sources/Attributes/validators/ValidatorFactory.swift
@@ -279,7 +279,7 @@ extension ValidatorFactory where Value: StringProtocol {
         push { $0.whitelist(list) }
     }
 
-    public func greylist(_ list: Set<String>) -> ValidatorFactory<Value> {
+    public func greyList(_ list: Set<String>) -> ValidatorFactory<Value> {
         push { $0.greyList(list) }
     }
 
