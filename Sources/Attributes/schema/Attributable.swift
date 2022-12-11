96e5e70a4b4b43fd20f880e7d40e7d846c1fbbeb
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-12 02:54:54 +1000

    Added comments to Attributable.

--- Sources/Attributes/schema/Attributable.swift
+++ Sources/Attributes/schema/Attributable.swift
@@ -56,45 +56,85 @@
  *
  */
 
+/// This protocol provides the bare-minimum implementation for a group of attributes
+/// that can be validated, triggered, and displayed. An instance of this protocol
+/// represents the types of attributes accessible through this interface. Additionally,
+/// this protocol defines the validators required for each attribute and all the attributes
+/// as a whole. A similar requirement is placed on the triggers, where an instance may define a
+/// trigger that reacts to target attributes within this protocol.
+/// 
+/// This protocol acts as the foundational protocol for the schema definitions. A schema is an
+/// implementation of this protocol with additional structures for types that combine grouped attributes and
+/// nested/recursive attributes. Each instance of Attributable is designed to work with a specific root
+/// object that is ``Modifiable``. Changing the data in the root object may require validation rules specified
+/// in this protocol and may trigger changes within the modifiable through the triggers defined in this
+/// protocol.
+/// 
+/// Furthermore, this protocol is defined to work with a root attribute that will typically be a
+/// ``BlockAttribute`` using nested attributes. The properties defined in this protocol should
+/// reflect the attributes within the root attribute and the paths should point to where these
+/// attributes are defined within the root object.
 public protocol Attributable {
 
+    /// The root object type that contains the data represented by this protocol.
     associatedtype Root: Modifiable
+
+    /// The type of the root Attribute.
     associatedtype AttributeRoot
+
+    /// The type of the path from the root to the root attribute.
     associatedtype SearchPath: ConvertibleSearchablePath where
         SearchPath.Root == Root, SearchPath.Value == AttributeRoot
 
+    /// A path to the root attribute contained within the root object.
     var path: SearchPath { get }
 
+    /// A path to the fields in the modifiable.
     var pathToFields: Path<AttributeRoot, [Field]> { get }
 
+    /// Path to any nested attributes within the root attribute.
     var pathToAttributes: Path<AttributeRoot, [String: Attribute]> { get }
 
+    /// All attributes within the root attribute. This property allows a declarative style of programming
+    /// where attributes are defined using property wrappers. This property will represent all attributes
+    /// contained within the root attribute.
     var properties: [SchemaAttribute] { get }
 
+    /// The validator for all of the properties.
     var propertiesValidator: AnyValidator<AttributeRoot> { get }
 
+    /// Additional triggers that are not defined for each attribute.
     var triggers: AnyTrigger<Root> { get }
 
+    /// All triggers within this protocol.
     var allTriggers: AnyTrigger<Root> { get }
 
+    /// A validation for the group containing the root attribute.
     var groupValidation: AnyValidator<AttributeRoot> { get }
 
+    /// The validator for the root object.
     var rootValidation: AnyValidator<Root> { get }
 
 }
 
+/// Add helper methods to all Attributable implementations.
 public extension Attributable {
 
+    /// ComplexCollectionProperty definition.
     typealias ComplexCollectionProperty<Base> = Attributes.ComplexCollectionProperty<Base> where
         Base: ComplexProtocol, Base.Root == AttributeRoot
 
+    /// ComplexProperty definition.
     typealias ComplexProperty<Base> = Attributes.ComplexProperty<Base> where
         Base: ComplexProtocol, Base.Root == AttributeRoot
 
+    /// No additional triggers by default.
     var triggers: AnyTrigger<Root> {
         AnyTrigger<Root>()
     }
 
+    /// All of the triggers are those defined in the attribute properties and the additional
+    /// triggers within the `triggers` property.
     var allTriggers: AnyTrigger<Root> {
         let mirror = Mirror(reflecting: self)
         let childTriggers: [AnyTrigger<Root>] = mirror.children.compactMap {
@@ -109,16 +149,22 @@ public extension Attributable {
         return AnyTrigger([triggers, AnyTrigger(childTriggers)])
     }
 
+    /// No validation by default.
     var groupValidation: AnyValidator<AttributeRoot> {
         AnyValidator<AttributeRoot>()
     }
 
+    /// No validation by default.
     var rootValidation: AnyValidator<Root> {
         AnyValidator<Root>()
     }
 
+    /// The properties of an instance of this protocol are created through dynamic member lookup
+    /// by default. This property will search the types within `Self` and find all properties that
+    /// use an attribute property wrapper.
     var properties: [SchemaAttribute] {
         let mirror = Mirror(reflecting: self)
+        // swiftlint:disable:next closure_body_length
         return mirror.children.compactMap {
             switch $0.value {
             case let val as BoolProperty:
@@ -155,6 +201,7 @@ public extension Attributable {
         }
     }
 
+    /// This property reduces all attribute property validators into a single validator.
     var propertiesValidator: AnyValidator<AttributeRoot> {
         let propertyValidators: [AnyValidator<AttributeRoot>] = properties.compactMap {
             $0.validate.toNewRoot(path: pathToAttributes[$0.label].wrappedValue)
@@ -162,6 +209,13 @@ public extension Attributable {
         return AnyValidator(propertyValidators)
     }
 
+    /// Find the property located at a path in the root object that is affected by this instance.
+    /// - Parameters:
+    ///   - path: The path of the attribute.
+    ///   - root: The root object containing the property.
+    /// - Returns: The property of nil if the path is invalid. This function will also return
+    /// nil for block attributes. It is assumed that the properties to locate exist within the
+    /// root attribute.
     func findProperty(path: AnyPath<Root>, in root: Root) -> SchemaAttribute? {
         guard let property = properties.first(where: {
             let searchPath = self.path(for: $0)
@@ -177,14 +231,27 @@ public extension Attributable {
         }
     }
 
+    /// Get a path for an attribute that exists within this type heirarchy.
+    /// - Parameter attribute: The attribute to locate.
+    /// - Returns: The path to the attribute to locate.
     func path(for attribute: SchemaAttribute) -> AnySearchablePath<Root, Attribute> {
         self.path.appending(path: self.pathToAttributes[attribute.label].wrappedValue)
     }
 
+    /// Get a path from the root object to an attribute that exists within the attributes dictionary.
+    /// - Parameters:
+    ///   - attribute: The attribute to locate.
+    ///   - path: A path from the root object to the attributes dictionary that contains all attributes.
+    /// - Returns: The new path from the root object to the attribute to locate.
     func path(for attribute: SchemaAttribute, in path: Path<Root, AttributeRoot>) -> Path<Root, Attribute> {
         pathToAttributes.changeRoot(path: path)[attribute.label].wrappedValue
     }
 
+    // swiftlint:disable identifier_name
+
+    /// Creates a trigger that is fired when an attribute changes.
+    /// - Parameter attribute: The attribute that causes the trigger to fire.
+    /// - Returns: A new trigger.
     func WhenChanged(_ attribute: SchemaAttribute) -> ForEach<
         AnySearchablePath<Self.Root, Attribute>,
         WhenChanged<Path<Self.Root, Attribute>,
@@ -195,19 +262,63 @@ public extension Attributable {
         }
     }
 
-    func WhenChanged<T>(_ value: Path<[LineAttribute], T>, in attribute: SchemaAttribute, perform: @escaping (inout Root) -> Result<Bool, AttributeError<Root>>) -> ForEach<Self.SearchPath, ForEach<CollectionSearchPath<Self.Root, [[LineAttribute]], T>, CustomTrigger<Path<CollectionSearchPath<Self.Root, [[LineAttribute]], T>.Root, CollectionSearchPath<Self.Root, [[LineAttribute]], T>.Value>>>> {
+    /// Creates a trigger that performs a custom trigger function when a table row is mutated.
+    /// - Parameters:
+    ///   - value: A path to the row that causes the new trigger to fire.
+    ///   - attribute: The table containing the row that causes this trigger to fire.
+    ///   - perform: The custom trigger function to enact when the trigger fires.
+    /// - Returns: The new trigger.
+    /// - Warning: Passing non-table values into attribute will cause a run-time crash.
+    func WhenChanged<T>(
+        _ value: Path<[LineAttribute], T>,
+        in attribute: SchemaAttribute,
+        perform: @escaping (inout Root) -> Result<Bool, AttributeError<Root>>
+    ) -> ForEach<
+        Self.SearchPath,
+        ForEach<
+            CollectionSearchPath<Self.Root, [[LineAttribute]], T>,
+            CustomTrigger<
+                Path<
+                    CollectionSearchPath<Self.Root, [[LineAttribute]], T>.Root,
+                    CollectionSearchPath<Self.Root, [[LineAttribute]], T>.Value
+                >
+            >
+        >
+    > {
         if !attribute.type.isTable {
             fatalError("Calling `WhenChanged(_:in:)` on attribute that is not a table property")
         }
         return ForEach(self.path) { groupPath in
             let attributePath = self.path(for: attribute, in: groupPath)
-            ForEach(CollectionSearchPath(collectionPath: attributePath.tableValue, elementPath: value)) { path in
+            ForEach(
+                CollectionSearchPath(collectionPath: attributePath.tableValue, elementPath: value)
+            ) { path in
                 Attributes.WhenChanged(path).custom(perform)
             }
         }
     }
 
-    func WhenTrue(_ attribute: SchemaAttribute, makeAvailable hiddenAttribute: SchemaAttribute) -> ForEach<Self.SearchPath, WhenChanged<Path<Self.Root, Attribute>, ConditionalTrigger<MakeAvailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>, Path<Self.Root, [String : Attribute]>>>>> {
+    /// Creates a trigger that makes an attribute available when a boolean attribute is set to true.
+    /// - Parameters:
+    ///   - attribute: The boolean attribute that fires the new trigger.
+    ///   - hiddenAttribute: The attribute to make available.
+    /// - Returns: The new trigger.
+    /// - Warning: Passing a non-boolean attribute into `attribute` will cause a runtime crash.
+    func WhenTrue(
+        _ attribute: SchemaAttribute, makeAvailable hiddenAttribute: SchemaAttribute
+    ) -> ForEach<
+        Self.SearchPath,
+        WhenChanged<
+            Path<Self.Root, Attribute>,
+            ConditionalTrigger<
+                MakeAvailableTrigger<
+                    Path<Self.Root, Attribute>,
+                    Path<Self.Root, [Field]>,
+                    Path<Self.Root, [String: Attribute]>
+                >
+            >
+        >
+    > {
         if attribute.type != .bool {
             fatalError("Calling `WhenTrue` when attributes type is not `bool`.")
         }
@@ -232,7 +343,27 @@ public extension Attributable {
         }
     }
 
-    func WhenFalse(_ attribute: SchemaAttribute, makeAvailable hiddenAttribute: SchemaAttribute) -> ForEach<Self.SearchPath, WhenChanged<Path<Self.Root, Attribute>, ConditionalTrigger<MakeAvailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>, Path<Self.Root, [String : Attribute]>>>>> {
+    /// Creates a trigger that makes an attribute available when a boolean attribute is set to false.
+    /// - Parameters:
+    ///   - attribute: The boolean attribute that fires the new trigger.
+    ///   - hiddenAttribute: The attribute to make available.
+    /// - Returns: The new trigger.
+    /// - Warning: Passing a non-boolean attribute into `attribute` will cause a runtime crash.
+    func WhenFalse(
+        _ attribute: SchemaAttribute, makeAvailable hiddenAttribute: SchemaAttribute
+    ) -> ForEach<
+        Self.SearchPath,
+        WhenChanged<
+            Path<Self.Root, Attribute>,
+            ConditionalTrigger<
+                MakeAvailableTrigger<
+                    Path<Self.Root, Attribute>,
+                    Path<Self.Root, [Field]>,
+                    Path<Self.Root, [String: Attribute]>
+                >
+            >
+        >
+    > {
         if attribute.type != .bool {
             fatalError("Calling `WhenTrue` when attributes type is not `bool`.")
         }
@@ -257,7 +388,23 @@ public extension Attributable {
         }
     }
 
-    func WhenTrue(_ attribute: SchemaAttribute, makeUnavailable hiddenAttribute: SchemaAttribute) -> ForEach<Self.SearchPath, WhenChanged<Path<Self.Root, Attribute>, ConditionalTrigger<MakeUnavailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>>>>> {
+    /// Creates a trigger that makes an attribute unavailable when a boolean attribute is set to true.
+    /// - Parameters:
+    ///   - attribute: The boolean attribute that fires the new trigger.
+    ///   - hiddenAttribute: The attribute to make unavailable.
+    /// - Returns: The new trigger.
+    /// - Warning: Passing a non-boolean attribute into `attribute` will cause a runtime crash.
+    func WhenTrue(
+        _ attribute: SchemaAttribute, makeUnavailable hiddenAttribute: SchemaAttribute
+    ) -> ForEach<
+        Self.SearchPath,
+        WhenChanged<
+            Path<Self.Root, Attribute>,
+            ConditionalTrigger<
+                MakeUnavailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>>
+            >
+        >
+    > {
         if attribute.type != .bool {
             fatalError("Calling `WhenTrue` when attributes type is not `bool`.")
         }
@@ -274,7 +421,21 @@ public extension Attributable {
         }
     }
 
-    func WhenFalse(_ attribute: SchemaAttribute, makeUnavailable hiddenAttribute: SchemaAttribute) -> ForEach<Self.SearchPath, WhenChanged<Path<Self.Root, Attribute>, ConditionalTrigger<MakeUnavailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>>>>> {
+    /// Creates a trigger that makes an attribute unavailable when a boolean attribute is set to false.
+    /// - Parameters:
+    ///   - attribute: The boolean attribute that fires the new trigger.
+    ///   - hiddenAttribute: The attribute to make unavailable.
+    /// - Returns: The new trigger.
+    /// - Warning: Passing a non-boolean attribute into `attribute` will cause a runtime crash.
+    func WhenFalse(
+        _ attribute: SchemaAttribute, makeUnavailable hiddenAttribute: SchemaAttribute
+    ) -> ForEach<
+        Self.SearchPath,
+        WhenChanged<
+            Path<Self.Root, Attribute>,
+            ConditionalTrigger<MakeUnavailableTrigger<Path<Self.Root, Attribute>, Path<Self.Root, [Field]>>>
+        >
+    > {
         if attribute.type != .bool {
             fatalError("Calling `WhenTrue` when attributes type is not `bool`.")
         }
@@ -290,5 +451,7 @@ public extension Attributable {
             }
         }
     }
+
+    // swiftlint:enable identifier_name
 
 }
