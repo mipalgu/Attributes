fe1c58da9b1e3ac5f05a6ee65be1317a0ec9c341
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-11 15:03:15 +1000

    added null object property for testing.

--- Tests/AttributesTests/schema/NullSchemaAttributeConvertible.swift
+++ Tests/AttributesTests/schema/NullSchemaAttributeConvertible.swift
@@ -1,4 +1,4 @@
-// SchemaProperties.swift
+// NullSchemaAttributeConvertible.swift
 // Attributes
 // 
 // Created by Morgan McColl.
@@ -54,85 +54,19 @@
 // Fifth Floor, Boston, MA  02110-1301, USA.
 // 
 
-import Attributes
+@testable import Attributes
 
-/// Container for testing all attribute types.
-struct SchemaProperties: Attributable {
+/// A null object representation of a ``SchemaAttributeConvertible``.
+struct NullSchemaAttributeConvertible: SchemaAttributeConvertible {
 
-    /// No data.
-    typealias Root = EmptyModifiable
+    /// Triggers are invalid.
+    var allTriggers: Any {
+        0
+    }
 
-    /// Attribute root.
-    typealias AttributeRoot = Attribute
-
-    /// Search path.
-    typealias SearchPath = Path<EmptyModifiable, Attribute>
-
-    /// Mock path, not used.
-    let path = Path(EmptyModifiable.self).attributes[0].attributes["bool"].wrappedValue
-
-    /// Mock path, not used.
-    let pathToFields = Path(Attribute.self).complexFields
-
-    /// Mock path, not used.
-    let pathToAttributes = Path(Attribute.self).complexValue
-
-    /// A null property.
-    let nullAttribute = NullSchemaAttributeConvertible()
-
-    /// A bool property.
-    @BoolProperty(label: "bool")
-    var bool
-
-    /// A code property.
-    @CodeProperty(label: "code", language: .swift)
-    var code
-
-    /// A collection property.
-    @CollectionProperty(label: "collection", bools: ValidatorFactory<Bool>.required())
-    var collection
-
-    /// A complex property.
-    @ComplexProperty(base: ComplexPerson(), label: "complex")
-    var complex
-
-    /// A complex collection property.
-    @ComplexCollectionProperty(base: ComplexPerson(), label: "complex_collection")
-    var complexCollection
-
-    /// An enumerable collection property.
-    @EnumerableCollectionProperty(label: "enumerable_collection", validValues: ["A", "B", "C"])
-    var enumerableCollection
-
-    /// An enumerated property.
-    @EnumeratedProperty(label: "enumerated", validValues: ["A", "B", "C"])
-    var enumerated
-
-    /// An expression property.
-    @ExpressionProperty(label: "expression", language: .swift)
-    var expression
-
-    /// A float property.
-    @FloatProperty(label: "float")
-    var float
-
-    /// An integer property.
-    @IntegerProperty(label: "integer")
-    var integer
-
-    /// A line property.
-    @LineProperty(label: "line")
-    var line
-
-    /// A table property.
-    @TableProperty(
-        label: "table",
-        columns: [TableColumn.bool(label: "bool", validation: ValidatorFactory<Bool>.required())]
-    )
-    var table
-
-    /// A text property.
-    @TextProperty(label: "text")
-    var text
+    /// Schema attribute is invalid.
+    var schemaAttribute: Any {
+        0
+    }
 
 }
